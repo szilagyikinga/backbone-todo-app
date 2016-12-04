08b14d289994035d10d98663dac8382e37d846a7
Kinga Szilagyi <kingas@bamlab.fr>
2016-12-04 16:33:29 +0100

    Enable todo item editing

--- js/views/todo.js
+++ js/views/todo.js
@@ -7,7 +7,32 @@ const TodoView = View.extend({
   template: todoTemplate,
   render: function(){
     this.$el.html(this.template(this.model.toJSON()));
+    this.input = this.$('.edit');
     return this;
+  },
+  initialize: function(){
+    this.model.on('change', this.render, this);
+  },
+  events: {
+    'dblclick label' : 'edit',
+    'keypress .edit' : 'updateOnEnter',
+    'blur .edit' : 'close'
+  },
+  edit: function(){
+    this.$el.addClass('editing');
+    this.input.focus();
+  },
+  close: function(){
+    var value = this.input.val().trim();
+    if(value) {
+      this.model.save({title: value});
+    }
+    this.$el.removeClass('editing');
+  },
+  updateOnEnter: function(e){
+    if(e.which == 13){
+      this.close();
+    }
   }
 });
 
