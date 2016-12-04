8dd660658312ed9f2b76c361a0383139585c1e17
Kinga Szilagyi <kingas@bamlab.fr>
2016-12-04 07:24:09 +0100

    Use underscore template

--- js/views/app.js
+++ js/views/app.js
@@ -1,12 +1,14 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
 
 const AppView = Backbone.View.extend({
   el: '#container',
+  template: _.template("<h3>Hello <%= who %></h3>"),
   initialize: function(){
     this.render();
   },
   render: function(){
-    this.$el.html("Hello World");
+    this.$el.html(this.template({who: 'Backbone!'}));
   }
 });
 
