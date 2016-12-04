ff5085a544962efe87018e5deffde1050f9c5f2d
Kinga Szilagyi <kingas@bamlab.fr>
2016-12-04 16:46:18 +0100

    Can delete todo

--- js/views/todo.js
+++ js/views/todo.js
@@ -12,12 +12,14 @@ const TodoView = View.extend({
   },
   initialize: function(){
     this.model.on('change', this.render, this);
+    this.model.on('destroy', this.remove, this);
   },
   events: {
     'dblclick label' : 'edit',
     'keypress .edit' : 'updateOnEnter',
     'blur .edit' : 'close',
-    'click .toggle': 'toggleCompleted'
+    'click .toggle': 'toggleCompleted',
+    'click .destroy': 'destroy'
   },
   edit: function(){
     this.$el.addClass('editing');
@@ -37,6 +39,9 @@ const TodoView = View.extend({
   },
   toggleCompleted: function(){
     this.model.toggle();
+  },
+  destroy: function(){
+    this.model.destroy();
   }
 });
 
